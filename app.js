85b5795bc25ae2ce32c83c57f55507a01aa1c735
Kingston Peng <qkpeng12@gmail.com>
2020-05-24 12:15:38 -0400

    Lavel 5 Authentication code

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const mongoose = require('mongoose');
 const app = express();
 // const encrypt = require("mongoose-encryption"); // level 2
 // const md5 = require('md5');   // level 3
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// const bcrypt = require('bcrypt');  // level 4
+// const saltRounds = 10;             // level 4
+const session = require('express-session')  // level 5
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 app.set('view engine', 'ejs');
 
@@ -19,22 +22,40 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+// use exporess-session
+app.use(session({
+  secret: "Our Little secret.",
+  resave: false,
+  saveUninitialized: false
+}))
+
+// use passport js
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// console.log(md5('123456'));
+// mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
 	email: String,
 	password: String
 });
 
-// const secret = "Thisisourlittlesecret."
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']  }); // before the "mongoose.model" line below
+// make sure userSchema is a mongoose.Schema, not just a JS object.
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -48,51 +69,51 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
-// User Register
-
-app.post("/register", function(req, res){
-
-  const hash = bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email:     req.body.username,
-      password:  hash
-    })
-
-    newUser.save(function(err){ // encrypt password
-      if (err) {
-        res.send(err);
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    })
-  });
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
 })
 
-// User Login Web page
-app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password
-  // decrypt password when read
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
+// User Register web page
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err){
       console.log(err)
+      res.redirect("/register")
     } else {
-      if (foundUser) {
-        // if ( password === foundUser.password ) {
-        if (bcrypt.compareSync(password, foundUser.password)) {   
-          res.render("secrets")
-        } else {
-          res.send("Incorrect password, please try again")
-        }
-      } else {
-        res.send("The user is not registered, please register before login.")
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      })
     }
   })
 })
 
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+})
 
+// User Login Web page
+app.post("/login", function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+// use passport
+  req.login(user, function(err){
+    if (err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      })
+    }
+  })
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
